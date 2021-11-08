9f27ed3c53d9c514b1a9ac75deb46c1f858e7bce
caudaganesh <caudaganesh@gmail.com>
2021-11-08 19:55:03 +0700

    update python example to handle salt length

--- digital-signature/python-example/digital-signature.py
+++ digital-signature/python-example/digital-signature.py
@@ -1,22 +1,24 @@
 from Crypto.Signature import pss
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
-from Crypto import Random
 import base64
 
 message = '{}'
+message_bytes = message.encode('utf-8')
+h = SHA256.new(message_bytes)
+
 key = RSA.import_key(open('../key/priv.pem').read())
-h = SHA256.new(message.encode('utf-8'))
-signature = pss.new(key).sign(h)
+l = int(key.n.bit_length() / 8) - h.digest_size-2
+signature = pss.new(key, salt_bytes=l).sign(h)
 
 encodedBytes = base64.b64encode(signature)
 encodedStr = str(encodedBytes, "utf-8")
 print(encodedStr)
 
 key = RSA.import_key(open('../key/pub.pem').read())
-verifier = pss.new(key)
+verifier = pss.new(key, salt_bytes=l)
 try:
     verifier.verify(h, signature)
     print("The signature is authentic.")
 except (ValueError, TypeError):
-    print("The signature is not authentic.")
+    print("The signature is NOT authentic.")
