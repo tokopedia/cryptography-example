4bc2827a79ee5179d801df6acacbdcd26fcf6a31
caudaganesh <caudaganesh@gmail.com>
2022-02-25 16:10:28 +0700

    chore(digital signature): update php example to handle verify with golang's default algo

--- digital-signature/php-example/main.php
+++ digital-signature/php-example/main.php
@@ -4,15 +4,28 @@ require __DIR__ . '/vendor/autoload.php';
 use phpseclib3\Crypt\RSA;
 $privKey = file_get_contents('../key/priv.pem');
 $pubKey = file_get_contents('../key/pub.pem');
-$priv = RSA::loadPrivateKey($privKey); // private key
+$priv = RSA::loadPrivateKey($privKey) // private key
+    ->withHash('sha256'); // hash algorithm
 
 $plaintext = 'test';
+
+$keySize = $priv->getLength() / 8; // key length in byte
+$hashed = hash('sha256', $plaintext, true); // hash with sha256 
+$hashLength = strlen($hashed); // length of hash 
+$saltLength = $keySize - 2 - $hashLength; // length of salt
+$priv = $priv->withSaltLength($saltLength);
+
 $signature = $priv->sign($plaintext);
 $base64sign = base64_encode($signature);
-
 echo $base64sign;
 
 $pub = RSA::loadPublicKey($pubKey);
+$keySize = $pub->getLength() / 8; // key length in byte
+$hashed = hash('sha256', $plaintext, true); // hash with sha256
+$hashLength = strlen(($hashed)); // length of hash
+$saltLength = $keySize - 2 - $hashLength; // length of salt
+$pub = $pub->withSaltLength($saltLength);
+
 $verify = $pub->verify($plaintext, $signature);
 
 echo "\n";
