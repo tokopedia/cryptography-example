c0a4f5457ca05f41ad25fd1c51e27b177da9df53
dafaath <78836795+dafaath@users.noreply.github.com>
2023-12-08 11:15:19 +0700

    fix(PHP): Adding example for signature PKCS1 on PHP example (#19)
    * adding example for signature PKCS1 on PHP signature

--- digital-signature/php-example/main.php
+++ digital-signature/php-example/main.php
@@ -2,10 +2,13 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use phpseclib3\Crypt\RSA;
+
 $privKey = file_get_contents('../key/priv.pem');
 $pubKey = file_get_contents('../key/pub.pem');
 $priv = RSA::loadPrivateKey($privKey) // private key
     ->withHash('sha256'); // hash algorithm
+// $priv = $priv->withPadding(RSA::SIGNATURE_PKCS1); // Add this code to change the padding to PKCS1, By default, the padding is RSA::SIGNATURE_PSS
+
 
 $plaintext = 'test';
 
@@ -25,9 +28,9 @@ $hashed = hash('sha256', $plaintext, true); // hash with sha256
 $hashLength = strlen(($hashed)); // length of hash
 $saltLength = $keySize - 2 - $hashLength; // length of salt
 $pub = $pub->withSaltLength($saltLength);
+// $pub = $pub->withPadding(RSA::SIGNATURE_PKCS1); // Add this code to change the padding to PKCS1, By default, the padding is RSA::SIGNATURE_PSS
 
 $verify = $pub->verify($plaintext, $signature);
 
 echo "\n";
 echo $verify ? 'verified' : 'unverified';
-?>
